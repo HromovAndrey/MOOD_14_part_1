895a50e859098e53b0ebf2a66735a6c6f7f37bdf
user <andrey.gromov.36912@gmail.com>
2024-05-07 21:48:14 +0300

    Завдання 2

--- Laba.py
+++ Laba.py
@@ -1,42 +1,27 @@
-# Завдання 3
-# Користувач вводить з клавіатури шлях до файлу, що
-# містить набір чисел. Після чого запускаються два потоки.
-# Перший потік створює новий файл, в який запише лише
-# парні елементи списку. Другий потік створює новий файл,
-# в який запише лише непарні елементи списку. Кількість
-# парних і непарних елементів виводиться на екран.
+# Завдання 4
+# Користувач вводить з клавіатури шлях до файлу та
+# слово для пошуку. Після чого запускається потік для
+# пошуку цього слова у файлі. Результат пошуку виведіть
+# на екран.
 
 import threading
-def even_numbers(path_file):
-    couples = []
-    with open(path_file, 'r') as file:
-        numbres = [int(number) for number in file.read().split()]
-        couples = [number for number in numbres if number % 2 == 0]
+def word_search(go_file, word):
+    search = False
+    with open(go_file, 'r') as file:
+        for line in file:
+            if word in line:
+                search = True
+                break
 
-    with open('парні_числа.txt', 'w') as new_file:
-        for number in couples:
-            new_file.write(str(number) + '\n')
+    if search:
+        print(f"Слово '{word}' знайдено у файлі.")
+    else:
+        print(f"Слово '{word}' не знайдено у файлі.")
 
-    print(f"Кількість парних чисел: {len(couples)}")
+go_file = input("Введіть шлях до файлу: ")
+word_search1 = input("Введіть слово для пошуку: ")
 
-def odd_numbers(path_file):
-    odd = []
-    with open(path_file, 'r') as file:
-        numbers = [int(number) for number in file.read().split()]
-        odd = [number for number in numbers if number % 2 != 0]
+p = threading.Thread(target=word_search, args=(go_file, word_search1))
+p.start()
 
-    with open('непарні_числа.txt', 'w') as new_file:
-        for number in odd:
-            new_file.write(str(number) + '\n')
-
-    print(f"Кількість непарних чисел: {len(odd)}")
-go_file = input("Введіть шлях до файлу з числами: ")
-
-p1 = threading.Thread(target=even_numbers, args=(go_file,))
-p1.start()
-
-p2 = threading.Thread(target=odd_numbers, args=(go_file,))
-p2.start()
-
-p1.join()
-p2.join()
+p.join()
