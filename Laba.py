93ae4216c0898c704504e8c38d19155f780032ba
user <andrey.gromov.36912@gmail.com>
2024-05-07 21:03:58 +0300

    Завдання 2

--- Laba.py
+++ Laba.py
@@ -1,24 +1,26 @@
-# Завдання 1
+# Завдання 2
 # Користувач вводить з клавіатури значення у список.
-# Після чого запускаються два потоки. Перший потік знаходить максимум у списку. Другий потік знаходить мінімум
-# у списку. Результати обчислень виведіть на екран.
+# Після чого запускаються два потоки. Перший потік знаходить суму елементів у списку. Другий потік знаходить
+# середнє арифметичне у списку. Результати обчислень
+# виведіть на екран.
 import threading
 
-def find_max(list):
-    maximum = max(list)
-    print(f"Максимум у списку: {maximum}")
+def find_suny(list):
+    suma = sum(list)
+    print(f"Сума елементів у списку: {suma}")
 
-def find_minimum(list):
-    minimum = min(list)
-    print(f"Мінімум у списку: {minimum}")
+def find_average(list):
+    avarage = sum(list) / len(list)
+    print(f"Середнє арифметичне у списку: {avarage}")
+
 
 entered_values = input("Введіть значення у список через пробіл: ").split()
 list = [int(x) for x in entered_values]
 
-p1 = threading.Thread(target=find_max, args=(list,))
+p1 = threading.Thread(target=find_suny, args=(list,))
 p1.start()
 
-p2 = threading.Thread(target=find_minimum, args=(list,))
+p2 = threading.Thread(target=find_average, args=(list,))
 p2.start()
 
 p1.join()
