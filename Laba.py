af135270497ac43ec3a6947049bcee5e81af5042
user <andrey.gromov.36912@gmail.com>
2024-05-07 21:33:46 +0300

    Завдання 2

--- Laba.py
+++ Laba.py
@@ -1,26 +1,41 @@
-# Завдання 2
-# Користувач вводить з клавіатури значення у список.
-# Після чого запускаються два потоки. Перший потік знаходить суму елементів у списку. Другий потік знаходить
-# середнє арифметичне у списку. Результати обчислень
-# виведіть на екран.
+# Завдання 3
+# Користувач вводить з клавіатури шлях до файлу, що
+# містить набір чисел. Після чого запускаються два потоки.
+# Перший потік створює новий файл, в який запише лише
+# парні елементи списку. Другий потік створює новий файл,
+# в який запише лише непарні елементи списку. Кількість
+# парних і непарних елементів виводиться на екран.
+
 import threading
+def even_numbers(path_file):
+    couples = []
+    with open(path_file, 'r') as file:
+        numbres = [int(number) for number in file.read().split()]
+        couples = [number for number in numbres if number % 2 == 0]
 
-def find_suny(list):
-    suma = sum(list)
-    print(f"Сума елементів у списку: {suma}")
+    with open('парні_числа.txt', 'w') as new_file:
+        for number in couples:
+            new_file.write(str(number) + '\n')
 
-def find_average(list):
-    avarage = sum(list) / len(list)
-    print(f"Середнє арифметичне у списку: {avarage}")
+    print(f"Кількість парних чисел: {len(couples)}")
 
+def odd_numbers(path_file):
+    odd = []
+    with open(path_file, 'r') as file:
+        numbers = [int(number) for number in file.read().split()]
+        odd = [number for number in numbers if number % 2 != 0]
 
-entered_values = input("Введіть значення у список через пробіл: ").split()
-list = [int(x) for x in entered_values]
+    with open('непарні_числа.txt', 'w') as new_file:
+        for number in odd:
+            new_file.write(str(number) + '\n')
 
-p1 = threading.Thread(target=find_suny, args=(list,))
+    print(f"Кількість непарних чисел: {len(odd)}")
+go_file = input("Введіть шлях до файлу з числами: ")
+
+p1 = threading.Thread(target=even_numbers, args=(go_file,))
 p1.start()
 
-p2 = threading.Thread(target=find_average, args=(list,))
+p2 = threading.Thread(target=odd_numbers, args=(go_file,))
 p2.start()
 
 p1.join()
